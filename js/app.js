81ef21c50873932bf24ed395f5c26a525c96bf42
abhishek <abhiskg20@gmail.com>
2022-08-23 14:08:00 +0600

    Displaying cart fn added

--- js/app.js
+++ js/app.js
@@ -2,13 +2,52 @@
 const allProducts = [];
 function addToCart(element) {
     var _a, _b;
-    element.setAttribute("disabled", "");
-    const productName = (_a = element.closest(".card-body")) === null || _a === void 0 ? void 0 : _a.children[0].textContent;
-    const productPrice = (_b = element.closest(".card-body")) === null || _b === void 0 ? void 0 : _b.children[1].children[0].textContent;
-    const product = {
-        productName: productName,
-        productPrice: productPrice,
-    };
-    allProducts.push(product);
-    console.log(allProducts);
+    const productCount = document.querySelector("#product-count");
+    const prevProductAdded = parseInt(productCount.innerText);
+    if (prevProductAdded < 3) {
+        const productName = (_a = element.closest(".card-body")) === null || _a === void 0 ? void 0 : _a.children[0].textContent;
+        const productPrice = (_b = element.closest(".card-body")) === null || _b === void 0 ? void 0 : _b.children[1].children[0].textContent;
+        if (productName === null || productName === undefined) {
+            alert("Product not avaiable");
+            return;
+        }
+        if (productPrice === null || productPrice === undefined) {
+            alert("Product not avaiable");
+            return;
+        }
+        element.setAttribute("disabled", "");
+        const product = {
+            productName: productName,
+            productPrice: parseInt(productPrice),
+        };
+        allProducts.push(product);
+        const totalProduct = allProducts.length;
+        productCount.innerText = totalProduct.toString();
+        displayInCart(allProducts);
+    }
+    else {
+        alert("Can't select more than 3 products");
+        return;
+    }
+}
+function displayInCart(products) {
+    const cartBody = document.querySelector("#cart-body");
+    cartBody.innerHTML = "";
+    let totalPrice = 0;
+    for (let i = 0; i < products.length; i++) {
+        const product = products[i];
+        const productName = product.productName;
+        const productPrice = product.productPrice;
+        totalPrice = totalPrice + productPrice;
+        const tr = document.createElement("tr");
+        tr.innerHTML = `<th>${i + 1}</th>
+      <td>${productName}</td>
+      <td>${productPrice}</td>`;
+        cartBody.append(tr);
+    }
+    const tr = document.createElement("tr");
+    tr.innerHTML = `<th></th>
+      <td>Total</td>
+      <td>${totalPrice}</td>`;
+    cartBody.append(tr);
 }
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,73 @@
-const allProducts: object[] = [];
+interface Products {
+  productName: string;
+  productPrice: number;
+}
+
+const allProducts: Products[] = [];
 
 function addToCart(element: HTMLButtonElement) {
-  element.setAttribute("disabled", "");
-  const productName = element.closest(".card-body")?.children[0].textContent;
-  const productPrice =
-    element.closest(".card-body")?.children[1].children[0].textContent;
-  const product = {
-    productName: productName,
-    productPrice: productPrice,
-  };
-  allProducts.push(product);
-  console.log(allProducts);
+  const productCount = document.querySelector(
+    "#product-count"
+  ) as HTMLSpanElement;
+  const prevProductAdded = parseInt(productCount.innerText);
+
+  if (prevProductAdded < 3) {
+    const productName = element.closest(".card-body")?.children[0].textContent;
+    const productPrice =
+      element.closest(".card-body")?.children[1].children[0].textContent;
+
+    if (productName === null || productName === undefined) {
+      alert("Product not avaiable");
+      return;
+    }
+
+    if (productPrice === null || productPrice === undefined) {
+      alert("Product not avaiable");
+      return;
+    }
+
+    element.setAttribute("disabled", "");
+
+    const product = {
+      productName: productName,
+      productPrice: parseInt(productPrice),
+    };
+
+    allProducts.push(product);
+
+    const totalProduct = allProducts.length;
+    productCount.innerText = totalProduct.toString();
+
+    displayInCart(allProducts);
+  } else {
+    alert("Can't select more than 3 products");
+    return;
+  }
+}
+
+function displayInCart(products: Products[]) {
+  const cartBody = document.querySelector(
+    "#cart-body"
+  ) as HTMLTableSectionElement;
+  cartBody.innerHTML = "";
+  let totalPrice = 0;
+
+  for (let i = 0; i < products.length; i++) {
+    const product = products[i];
+    const productName = product.productName;
+    const productPrice = product.productPrice;
+
+    totalPrice = totalPrice + productPrice;
+    const tr = document.createElement("tr");
+    tr.innerHTML = `<th>${i + 1}</th>
+      <td>${productName}</td>
+      <td>${productPrice}</td>`;
+
+    cartBody.append(tr);
+  }
+  const tr = document.createElement("tr");
+  tr.innerHTML = `<th></th>
+      <td>Total</td>
+      <td>${totalPrice}</td>`;
+  cartBody.append(tr);
 }
